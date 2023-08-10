9e1156658da18e0553256be2d8fc48e939084dd8
Fractal-Tess <vgfractal@gmail.com>
2023-08-10 18:23:40 +0300

    suppress hydr warnings when using grammarly extension

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,8 +6,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-base-100 text-base-content min-h-screen">
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        suppressHydrationWarning={true}
+        className="bg-base-100 text-base-content min-h-screen"
+      >
         {children}
       </body>
     </html>
