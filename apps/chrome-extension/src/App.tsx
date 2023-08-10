53348c391bb2bf4b3baea64d556f4dd2fe058474
Fractal-Tess <vgfractal@gmail.com>
2023-08-11 02:39:53 +0300

    chrome extension scafold

---
+++ apps/chrome-extension/src/App.tsx
@@ -0,0 +1,44 @@
+import {
+  Avatar,
+  AvatarImage,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  buttonVariants,
+} from 'ui';
+
+import { GithubIcon } from 'lucide-react';
+
+function App() {
+  return (
+    <main className="h-96 w-96 form-control items-center justify-center">
+      <Card>
+        <CardHeader>
+          <div className="flex justify-center mb-6">
+            <Avatar className="w-14 h-14">
+              <AvatarImage src="/icon.png" className="rounded-full" />
+            </Avatar>
+          </div>
+          <CardTitle>Project-S</CardTitle>
+          <CardDescription>Project-S's chrome extension </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="text-2xl font-bold">In development</p>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/Fractal-Tess/project-s/tree/main/apps/chrome-extension"
+            className={buttonVariants({ variant: 'outline' })}
+          >
+            <GithubIcon />
+            Track development
+          </a>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
+
+export default App;
