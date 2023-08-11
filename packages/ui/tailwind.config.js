723a0937ba4fbd9a3730d4baffd5d1e994f36b4d
Fractal-Tess <vgfractal@gmail.com>
2023-08-11 04:09:58 +0300

    custom themes

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: ['../../packages/ui/components/**/*.{ts,tsx}'],
   theme: {
     container: {
@@ -33,9 +33,25 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
+          primary: '#45B1E8',
+          secondary: '#fbbf24',
+          error: '#ec223d',
+        },
+        dark: {
+          ...require('daisyui/src/theming/themes')['[data-theme=black]'],
+          primary: '#45B1E8',
+          secondary: '#fbbf24',
+          error: '#ec223d',
+        },
+      },
+    ],
+    // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: 'dark', // name of one of the included themes for dark mode
-    // base: true, // applies background color and foreground color for root element by default
+    base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
