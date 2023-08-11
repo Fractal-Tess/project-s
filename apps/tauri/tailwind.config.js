f6378bbc8ad69ac10a4d3e77ab02c57c521f4d9a
Fractal-Tess <vgfractal@gmail.com>
2023-08-11 04:08:48 +0300

    test ui

--- apps/tauri/tailwind.config.js
+++ apps/tauri/tailwind.config.js
@@ -1,8 +1,8 @@
 import config from 'ui/tailwind.config';
 
+console.log(config.daisyui);
 /** @type {import('tailwindcss').Config} */
 export default {
   ...config,
-
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', ...config.content],
 };
