51ef35c31b891700a13a050b92a3a46305dfaa07
Fractal-Tess <vgfractal@gmail.com>
2023-08-13 23:53:49 +0300

    update configs

--- apps/tauri/vite.config.ts
+++ apps/tauri/vite.config.ts
@@ -5,16 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(async () => ({
   plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  build: {
+    outDir: 'build',
+  },
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 5173,
     strictPort: true,
   },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+
   envPrefix: ['VITE_', 'TAURI_'],
 }));
