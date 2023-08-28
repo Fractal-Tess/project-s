5c3b151c8905205ce7e433a16fe6ae851f17cc06
Fractal-Tess <vgfractal@gmail.com>
2023-08-29 00:25:51 +0300

    fix errors

--- apps/tauri/src/App.tsx
+++ apps/tauri/src/App.tsx
@@ -1,24 +1,10 @@
 import { Button } from 'ui';
-import { loadDefault } from './ipc';
-import { addSnippet, db } from './db';
-import { path } from '@tauri-apps/api';
 function App() {
-  db.execute(
-    'CREATE TABLE IF NOT EXISTS snippets (id INTEGER PRIMARY KEY);'
-  ).then(console.log);
-  path.appDir().then(console.log);
-  async function handler() {
-    addSnippet();
-    console.log('here');
-    // localStorage.getItem('e')?.split('');
-    // await loadDefault();
-  }
-
   return (
     <>
       {/* Header */}
       <main className="form-control justify-center flex-1 items-center">
-        <Button onClick={handler}>Load Default</Button>
+        <Button>Load Default</Button>
       </main>
       {/* Footer */}
     </>
